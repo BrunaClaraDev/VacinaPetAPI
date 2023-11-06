48c70ef151277b4899661dc4e1db03b9548851c6
Bruna Clara Basilio <bruna.clara@squadra.com.br>
2023-11-06 19:32:47 -0300

    Tratamento de erro na controller

--- Controllers/VacinasController.cs
+++ Controllers/VacinasController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,27 +13,37 @@ namespace Back_VacinaPet.Controllers
         private readonly IVacinaRepositorio _vacinaRepositorio;
         private readonly IPetRepositorio _petRepositorio;
         private readonly IValidarVacinas _validarVacinas;
+        private readonly ILogger<VacinasController> _logger;
 
-        public VacinasController(IVacinaRepositorio vacinaRepositorio, IPetRepositorio petRepositorio, IValidarVacinas validarVacinas)
+        public VacinasController(IVacinaRepositorio vacinaRepositorio, IPetRepositorio petRepositorio, IValidarVacinas validarVacinas, ILogger<VacinasController> logger)
         {
             _vacinaRepositorio = vacinaRepositorio;
             _petRepositorio = petRepositorio;
             _validarVacinas = validarVacinas;
+            _logger = logger;
         }
 
         [HttpPost]
         public async Task<List<Vacina>> GetVacinasAsync(Pet pet)
         {
-            var vacinasTomar = _validarVacinas.PegaVacinaPet(pet);
-            await Task.WhenAll(
-                _petRepositorio.SalvarPetAsync(pet)
-                );
-            await Task.WhenAll(
-               _vacinaRepositorio.SalvarVacinasAsync(vacinasTomar)
-               );
+            try
+            {
+                List<Vacina> vacinasTomar = _validarVacinas.PegaVacinaPet(pet);
+                await Task.WhenAny(
+                    _petRepositorio.SalvarPetAsync(pet)
+                    );
+                await Task.WhenAny(
+                   _vacinaRepositorio.SalvarVacinasAsync(vacinasTomar)
+                   );
 
+                return vacinasTomar;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Erro: {ex.Message}");
+                throw;
+            }
 
-            return vacinasTomar;
         }
     }
 }
